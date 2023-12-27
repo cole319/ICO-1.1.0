f4a49e4e485f4abd893efa4103d4a0206cff6996
Suryarghya <suryar1332001@gmail.com>
2023-12-27 21:07:18 +0530

    index cleaned

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
-// import "./App.css";
 import dotenv from "dotenv";
 dotenv.config();
 
 import { CiMoneyBill } from "react-icons/ci";
 
-// import artifact from "./artifacts/contracts/BridgeCoinSale.sol/BridgeCoinSale.json";
 import artifact from "../artifacts/contracts/BridgeCoinSale.sol/BridgeCoinSale.json";
 const CONTRACT_ADDRESS = "0x669e629Df706BA32C6aB53f1EA7fb2DD51B517d1";
 
@@ -98,62 +96,7 @@ function App() {
     }
   };
 
-  // const connect = async () => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   setProvider(provider);
-  //   await provider.send("eth_requestAccounts", []);
-  //   const signer = provider.getSigner();
-  //   setSigner(signer);
-  //   signer.getAddress().then((address) => {
-  //     setSignerAddress(address);
-  //   });
-  // };
-
-  // const onLoad = async () => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   setProvider(provider);
-  //   await provider.send("eth_requestAccounts", []);
-
-  //   const signer = provider.getSigner();
-  //   setSigner(signer);
-
-  //   signer.getAddress().then((address) => {
-  //     setSignerAddress(address);
-  //   });
-  //   // const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   // await provider.send("eth_accounts", []);
-  //   // // setProvider(provider);
-
-  //   // if (provider !== undefined) {
-  //   //   setProvider(provider);
-  //   // }
-
-  //   // if (signer !== undefined) {
-  //   //   setSigner(signer);
-  //   // }
-
-  //   // signer.getAddress().then((address) => {
-  //   //   setSignerAddress(address);
-  //   // });
-
-  //   const contract = new ethers.Contract(
-  //     CONTRACT_ADDRESS,
-  //     artifact.abi,
-  //     provider
-  //   );
-
-  //   setContract(contract);
-  // };
-
   const isConnected = () => signer !== undefined;
-
-  // const getSigner = async (signer) => {
-  //   signer.getAddress().then((address) => {
-  //     setSignerAddress(address);
-  //   });
-
-  // return signer;
-  // };
 
   const toWei = (ether) => ethers.utils.parseEther(ether);
 
